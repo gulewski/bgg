a499d571a4352dd73dfa6640a967007255b234e2
gulewski <gulewski.ae@gmail.com>
2020-08-23 20:15:00 +0200

    finished function that updates sublists of entities. planned next steps

--- fill_database.py
+++ fill_database.py
@@ -36,51 +36,99 @@ def get_ids_list(active_driver, page_num: int):
                 return _
         return None
 
+    # in progress
+    def get_description():
+        pass
+
     active_driver.get(creds.BGG_BROWSE_PAGE + str(page_num))
     table_rows = active_driver.find_elements_by_class_name("collection_objectname")
     id_list = []
+    descriptions = {}
     # if you like comprehensions, uncomment this
     # id_list = [get_obj_id(row.find_element_by_tag_name("a").get_attribute("href")) for row in table_rows]
     for row in table_rows:
         game_link = row.find_element_by_tag_name("a").get_attribute("href")
-        id_list.append(get_obj_id(game_link))
+        id = get_obj_id(game_link)
+        id_list.append(id)
+        # in progress
+        descriptions[id] = get_description()
     return id_list
 
 
+# updating sublists for core tables and connection between core table and boardgames table
+def update_sublists(conn_keys: dict, pure_table: list, bg_conn_table: list, bg):
+    for tag in conn_keys.keys():
+        type_searching = bg.find_all(tag)
+        for element in type_searching:
+            try:
+                if element["inbound"]:
+                    continue
+            except KeyError:
+                pure_table.append(tuple((element["objectid"], element.text)))
+                # a crutch for integrations
+                if tag == "boardgameintegration":
+                    bg_conn_table.append(tuple((bg["objectid"], element["objectid"], conn_keys[tag], "boardgame")))
+                else:
+                    bg_conn_table.append(tuple((bg["objectid"], element["objectid"], conn_keys[tag], tag)))
+
+
 # in progress
-def create_dictionary_people(bg):
-    type_link_conn = {"boardgamedesigner": "designer",
-                      "boardgameartist": "artist",
-                      "boardgamepublisher": "publisher",
-                      }
-    designers = bg.find_all("boardgamedesigner")
-    designers_dict = {}
-    for _ in designers:
-        designers_dict[_["objectid"]]= _.text
-    artists = bg.find_all("boardgameartist")
-    artists_dict = {}
-    for _ in artists:
-        artists_dict[_["objectid"]] = _.text
-    publishers = bg.find_all("boardgamepublisher")
-    publishers_dict = {}
-    for _ in publishers:
-        artists_dict[_["objectid"]] = _.text
-    print(designers_dict, artists_dict, publishers_dict)
+# updating sublist for boardgames itself
+# remember to take description out of table
+def update_bgames(bgames_table: list):
     pass
 
 # start of work
 time_start = datetime.datetime.now()
 driver = webdriver.Chrome(creds.WEBDRIVER_PATH)
 
+people_type_link_conn = {"boardgamedesigner": "designer",
+                         "boardgameartist": "artist",
+                         "boardgamepublisher": "publisher",
+                         }
+tags_type_link_conn = {"boardgamecategory": "category",
+                       "boardgamemechanic": "mechanic",
+                       "boardgamefamily": "family",
+                       "boardgamesubdomain": "subdomain",
+                       }
+additionals_type_link_conn = {"boardgameversion": "version",
+                              "boardgamehonor": "honor",
+                              "boardgamepodcastepisode": "podcast",
+                              "boardgameaccessory": "accessory",
+                              }
+related_type_link_conn = {"boardgameintegration": "integration",
+                          "boardgameexpansion": "expansion",
+                          }
+
 for page in range(1, 2):  # get_final_page(driver)+1):
-    games_data = {}
+    # entities for people and connections with boardgdames
+    people, bgames_people = [], []
+    # entities for tags and connections with boardgdames
+    tags, bgames_tags = [], []
+    # entities for additionals and connections with boardgdames
+    additionals, bgames_additionals = [], []
+    # entities for related games and connections with boardgdames
+    related, bgames_related = [], []
+    # getting list of bg ids for current page
     id_list = get_ids_list(driver, page)
+    # creating URI for xmlapi request
     xmlapi_ref = creds.XMLAPI_START + ",".join(id_list) + creds.XMLAPI_END
+    # get xmlapi page itself and put it in soup-object
     xmlapi_page = requests.get(xmlapi_ref)
     soup_for_games = BeautifulSoup(xmlapi_page.text, features="lxml")
+    # creating a list of boardgames for current xmlapi page
     boardgames = soup_for_games.find_all('boardgame')
-    for boardgame in boardgames:
-        create_dictionary_people(boardgame)
+    # for each boardgame updating sublists of entities
+    for boardgame in boardgames[:5]:
+        update_sublists(people_type_link_conn, people, bgames_people, boardgame)
+        update_sublists(tags_type_link_conn, tags, bgames_tags, boardgame)
+        update_sublists(additionals_type_link_conn, additionals, bgames_additionals, boardgame)
+        update_sublists(related_type_link_conn, related, bgames_related, boardgame)
+    print(f"people: {len(people)}, {len(set(people))}, {len(bgames_people)}\n"
+          f"tags: {len(tags)}, {len(set(tags))}, {len(bgames_tags)}\n"
+          f"additionals: {len(additionals)}, {len(set(additionals))}, {len(bgames_additionals)}\n"
+          f"related: {len(related)}, {len(set(related))}, {len(bgames_related)}\n"
+          f"{bgames_related[:3]}")
     #
     # for game_id in id_list:
     #     games_data[game_id] = {
