dc010a537b955853ad428ee8ee2690fe12c0cbe8
gulewski <gulewski.ae@gmail.com>
2020-09-05 21:18:51 +0200

    added __init__ and changed __repr__ in connection classes-tables

--- db_entities.py
+++ db_entities.py
@@ -41,184 +41,6 @@ class Artists(Base):
                f"Link: {self.link}"
 
 
-class Bgames(Base):
-    __tablename__ = "bgames"
-    bgame_id = Column(Integer, primary_key=True)
-    title = Column(String)
-    yearpublished = Column(Integer)
-    min_players = Column(Integer)
-    max_players = Column(Integer)
-    playtime = Column(Integer)
-    min_playtime = Column(Integer)
-    max_playtime = Column(Integer)
-    age = Column(Integer)
-    thumbnail = Column(String)
-    image = Column(String)
-    description = Column(String)
-    rank = Column(Integer)
-    usersrated = Column(Integer)
-    average = Column(Numeric)
-    bayesaverage = Column(Numeric)
-
-    def __repr__(self):
-        return f"Bgames. " \
-               f"ID: {self.bgame_id}, " \
-               f"title: {self.title}, " \
-               f"year: {self.year}, " \
-               f"min and max players: {self.min_players} {self.max_players}, " \
-               f"playtime, min and max: {self.playtime} {self.min_playtime} {self.max_playtime}, " \
-               f"age: {self.age}, " \
-               f"thumbnail and image: {self.thumbnail} {self.image}, " \
-               f"start of description: {self.description[:25]}, " \
-               f"end of description: {self.description[-25:]}, " \
-               f"rank: {self.rank}, " \
-               f"usersrated: {self.userrated}, " \
-               f"averages: {self.average} {self.bayesaverage}"
-
-
-class BgamesAccessories(Base):
-    __tablename__ = "bgames_accessories"
-    bgame_id = Column(Integer, primary_key=True)
-    accessory_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesAccessories. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Accessory ID: {self.accessory_id}"
-
-
-class BgamesArtists(Base):
-    __tablename__ = "bgames_artists"
-    bgame_id = Column(Integer, primary_key=True)
-    artist_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesArtists. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Artist ID: {self.artist_id}"
-
-
-class BgamesCategories(Base):
-    __tablename__ = "bgames_categories"
-    bgame_id = Column(Integer, primary_key=True)
-    category_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesCategories. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Category ID: {self.category_id}"
-
-
-class BgamesDesigners(Base):
-    __tablename__ = "bgames_designers"
-    bgame_id = Column(Integer, primary_key=True)
-    designer_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesDesigners. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Designer ID: {self.designer_id}"
-
-
-class BgamesExpansions(Base):
-    __tablename__ = "bgames_expansions"
-    bgame_id = Column(Integer, primary_key=True)
-    expansion_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesExpansions. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Expansion ID: {self.expansion_id}"
-
-
-class BgamesFamilies(Base):
-    __tablename__ = "bgames_families"
-    bgame_id = Column(Integer, primary_key=True)
-    family_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesFamilies. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Family ID: {self.family_id}"
-
-
-class BgamesHonors(Base):
-    __tablename__ = "bgames_honors"
-    bgame_id = Column(Integer, primary_key=True)
-    honor_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesHonors. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Honor ID: {self.honor_id}"
-
-
-class BgamesIntegrations(Base):
-    __tablename__ = "bgames_integrations"
-    bgame_id = Column(Integer, primary_key=True)
-    integration_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesIntegrations. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Integration ID: {self.integration_id}"
-
-
-class BgamesMechanics(Base):
-    __tablename__ = "bgames_mechanics"
-    bgame_id = Column(Integer, primary_key=True)
-    mechanic_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesMechanics. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Mechanic ID: {self.mechanic_id}"
-
-
-class BgamesPodcasts(Base):
-    __tablename__ = "bgames_podcasts"
-    bgame_id = Column(Integer, primary_key=True)
-    podcast_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesPodcasts. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Podcast ID: {self.podcast_id}"
-
-
-class BgamesPublishers(Base):
-    __tablename__ = "bgames_publishers"
-    bgame_id = Column(Integer, primary_key=True)
-    publisher_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesPublishers. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Publisher ID: {self.publisher_id}"
-
-
-class BgamesSubdomains(Base):
-    __tablename__ = "bgames_subdomains"
-    bgame_id = Column(Integer, primary_key=True)
-    subdomain_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesSubdomains. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Subdomain ID: {self.subdomain_id}"
-
-
-class BgamesVersions(Base):
-    __tablename__ = "bgames_versions"
-    bgame_id = Column(Integer, primary_key=True)
-    version_id = Column(Integer, primary_key=True)
-
-    def __repr__(self):
-        return f"BgamesVersions. " \
-               f"Bgame ID: {self.bgame_id}, " \
-               f"Version ID: {self.version_id}"
-
-
 class Categories(Base):
     __tablename__ = "categories"
     category_id = Column(Integer, primary_key=True)
@@ -415,3 +237,233 @@ class Versions(Base):
                f"ID: {self.version_id},\n" \
                f"Name: {self.version_name},\n" \
                f"Link: {self.link}"
+
+
+class Bgames(Base):
+    __tablename__ = "bgames"
+    bgame_id = Column(Integer, primary_key=True)
+    title = Column(String)
+    yearpublished = Column(Integer)
+    min_players = Column(Integer)
+    max_players = Column(Integer)
+    playtime = Column(Integer)
+    min_playtime = Column(Integer)
+    max_playtime = Column(Integer)
+    age = Column(Integer)
+    thumbnail = Column(String)
+    image = Column(String)
+    description = Column(String)
+    rank = Column(Integer)
+    usersrated = Column(Integer)
+    average = Column(Numeric)
+    bayesaverage = Column(Numeric)
+
+    def __repr__(self):
+        return f"Bgames.\n" \
+               f"ID: {self.bgame_id},\n" \
+               f"title: {self.title},\n" \
+               f"year: {self.year},\n" \
+               f"min and max players: {self.min_players} {self.max_players},\n" \
+               f"playtime, min and max: {self.playtime} {self.min_playtime} {self.max_playtime},\n" \
+               f"age: {self.age},\n" \
+               f"thumbnail and image: {self.thumbnail} {self.image},\n" \
+               f"start of description: {self.description[:25]},\n" \
+               f"end of description: {self.description[-25:]},\n" \
+               f"rank: {self.rank},\n" \
+               f"usersrated: {self.userrated},\n" \
+               f"averages: {self.average} {self.bayesaverage}"
+
+
+class BgamesAccessories(Base):
+    __tablename__ = "bgames_accessories"
+    bgame_id = Column(Integer, primary_key=True)
+    accessory_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, accessory_id):
+        self.bgame_id = bgame_id
+        self.accessory_id = accessory_id
+
+    def __repr__(self):
+        return f"BgamesAccessories.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Accessory ID: {self.accessory_id}"
+
+
+class BgamesArtists(Base):
+    __tablename__ = "bgames_artists"
+    bgame_id = Column(Integer, primary_key=True)
+    artist_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, artist_id):
+        self.bgame_id = bgame_id
+        self.artist_id = artist_id
+
+    def __repr__(self):
+        return f"BgamesArtists.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Artist ID: {self.artist_id}"
+
+
+class BgamesCategories(Base):
+    __tablename__ = "bgames_categories"
+    bgame_id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, category_id):
+        self.bgame_id = bgame_id
+        self.category_id = category_id
+
+    def __repr__(self):
+        return f"BgamesCategories.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Category ID: {self.category_id}"
+
+
+class BgamesDesigners(Base):
+    __tablename__ = "bgames_designers"
+    bgame_id = Column(Integer, primary_key=True)
+    designer_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, designer_id):
+        self.bgame_id = bgame_id
+        self.designer_id = designer_id
+
+    def __repr__(self):
+        return f"BgamesDesigners.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Designer ID: {self.designer_id}"
+
+
+class BgamesExpansions(Base):
+    __tablename__ = "bgames_expansions"
+    bgame_id = Column(Integer, primary_key=True)
+    expansion_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, expansion_id):
+        self.bgame_id = bgame_id
+        self.expansion_id = expansion_id
+
+    def __repr__(self):
+        return f"BgamesExpansions.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Expansion ID: {self.expansion_id}"
+
+
+class BgamesFamilies(Base):
+    __tablename__ = "bgames_families"
+    bgame_id = Column(Integer, primary_key=True)
+    family_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, family_id):
+        self.bgame_id = bgame_id
+        self.family_id = family_id
+
+    def __repr__(self):
+        return f"BgamesFamilies.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Family ID: {self.family_id}"
+
+
+class BgamesHonors(Base):
+    __tablename__ = "bgames_honors"
+    bgame_id = Column(Integer, primary_key=True)
+    honor_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, honor_id):
+        self.bgame_id = bgame_id
+        self.honor_id = honor_id
+
+    def __repr__(self):
+        return f"BgamesHonors.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Honor ID: {self.honor_id}"
+
+
+class BgamesIntegrations(Base):
+    __tablename__ = "bgames_integrations"
+    bgame_id = Column(Integer, primary_key=True)
+    integration_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, integration_id):
+        self.bgame_id = bgame_id
+        self.integration_id = integration_id
+
+    def __repr__(self):
+        return f"BgamesIntegrations.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Integration ID: {self.integration_id}"
+
+
+class BgamesMechanics(Base):
+    __tablename__ = "bgames_mechanics"
+    bgame_id = Column(Integer, primary_key=True)
+    mechanic_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, mechanic_id):
+        self.bgame_id = bgame_id
+        self.mechanic_id = mechanic_id
+
+    def __repr__(self):
+        return f"BgamesMechanics.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Mechanic ID: {self.mechanic_id}"
+
+
+class BgamesPodcasts(Base):
+    __tablename__ = "bgames_podcasts"
+    bgame_id = Column(Integer, primary_key=True)
+    podcast_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, podcast_id):
+        self.bgame_id = bgame_id
+        self.podcast_id = podcast_id
+
+    def __repr__(self):
+        return f"BgamesPodcasts.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Podcast Episode ID: {self.podcast_id}"
+
+
+class BgamesPublishers(Base):
+    __tablename__ = "bgames_publishers"
+    bgame_id = Column(Integer, primary_key=True)
+    publisher_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, publisher_id):
+        self.bgame_id = bgame_id
+        self.publisher_id = publisher_id
+
+    def __repr__(self):
+        return f"BgamesPublishers.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Publisher ID: {self.publisher_id}"
+
+
+class BgamesSubdomains(Base):
+    __tablename__ = "bgames_subdomains"
+    bgame_id = Column(Integer, primary_key=True)
+    subdomain_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, subdomain_id):
+        self.bgame_id = bgame_id
+        self.subdomain_id = subdomain_id
+
+    def __repr__(self):
+        return f"BgamesSubdomains.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Subdomain ID: {self.subdomain_id}"
+
+
+class BgamesVersions(Base):
+    __tablename__ = "bgames_versions"
+    bgame_id = Column(Integer, primary_key=True)
+    version_id = Column(Integer, primary_key=True)
+
+    def __init__(self, bgame_id, version_id):
+        self.bgame_id = bgame_id
+        self.version_id = version_id
+
+    def __repr__(self):
+        return f"BgamesVersions.\n" \
+               f"Bgame ID: {self.bgame_id},\n" \
+               f"Version ID: {self.version_id}"
